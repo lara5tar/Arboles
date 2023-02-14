00407f938a7dfb565c531d2893b943422f4da6fb
Ana Patricia Hernandez Lara <122854097+NugetDePatto@users.noreply.github.com>
2023-02-13 19:02:47 -0600

    eliminar

--- src/arboles/Arboles.java
+++ src/arboles/Arboles.java
@@ -1,7 +1,6 @@
 package arboles;
 
 import java.util.LinkedList;
-import java.util.NoSuchElementException;
 
 public class Arboles {
 
@@ -26,24 +25,24 @@ public class Arboles {
         return aux;
     }
 
-    void imprimirorden(Nodo nodoizq, Nodo nododer) {
-        if (nodoizq != null ) {
-            System.out.print(nodoizq + " " + nododer + " ");
-            imprimirorden(nodoizq.izq, nodoizq.der);
 
-            imprimirorden(nododer.izq, nododer.der);
-        } else if (nododer != null){
-            
+    //imprime en orden
+    void imprimirorden(Nodo nodo) {
+        if (nodo != null ) {
+            imprimirorden(nodo.izq);
+            System.out.println(nodo);
+            imprimirorden(nodo.der);
         }
     }
-    
-    
+
+
+    //imprime por nivel
     void imprimir() {
         LinkedList<Nodo> cola = new LinkedList<Nodo>();
-        
+
         if (raiz != null) {
             cola.add(raiz);
-            
+
                 while (!cola.isEmpty()) {
                     if (cola.getFirst().izq != null) {
                         cola.add(cola.getFirst().izq);
@@ -54,16 +53,78 @@ public class Arboles {
                     }
 
                     System.out.print(cola.poll().num + " ");
-                    
+
                 }
         }
 
         System.out.println("");
 
     }
-    
+
     void eliminar(){
-        
+        eliminar(raiz, 25);
+    }
+
+    Nodo eliminar(Nodo aux, int num) {
+        if (num == -1) {
+            if(aux.der != null){
+                aux = eliminar(aux.der, -1);
+            }
+        } else if (aux == null) {
+            System.out.println("No se encontro el numero...");
+            return null;
+        } else if (aux.num == num) {//aqui encuentra el nodo a eliminar...
+            if (aux.izq != null) {
+                if(aux.der == null){
+                    return aux.izq;
+                } else {
+                    Nodo izq = aux.izq, der = aux.der;
+                    aux = eliminar(aux.izq, -1); 
+                    
+                    Nodo hijor = aux.izq;
+                    aux.izq = izq;
+                    aux.der = der;
+                    
+                    while(izq.der != aux){
+                        izq = izq.der;
+                    }
+                    
+                    izq.der = hijor;
+                    
+                }// aqui termina
+            } else if (aux.der != null) {
+                return aux.der;
+            } else {
+                System.out.println("hoja");
+                return null;
+            }
+        } else if (num < aux.num) { //estos dos metods buscan el nodo a eliminar
+            aux.izq = eliminar(aux.izq, num);
+        } else {
+            aux.der = eliminar(aux.der, num);
+        }
+        return aux;
     }
 
 }
+/*
+                Nodo nuevo = aux.izq.der;
+                Nodo izq = aux.izq;
+                Nodo der = aux.der;
+                aux = eliminar(aux.izq, -1);
+                Nodo izqder = aux.izq;
+                aux.izq = izq;
+                aux.der = der;
+                aux.izq.der = nuevo;
+                
+                Nodo auxaux = aux.izq;
+                while(auxaux.der != null){
+                    auxaux = auxaux.der;
+                }
+                System.out.println(aux.der);
+                System.out.println(auxaux);
+                if(aux.der != null){
+                    auxaux.der = izqder;
+                }
+                
+                 */
--- src/arboles/main.java
+++ src/arboles/main.java
@@ -7,10 +7,23 @@ public class main {
         a.insertar(50);
         a.insertar(25);
         a.insertar(75);
+        a.insertar(10);
+        a.insertar(35);
+        a.insertar(60);
         a.insertar(90);
-        a.insertar(26);
+        a.insertar(5);
+        a.insertar(20);
+        a.insertar(30);
+        a.insertar(40);
+        a.insertar(55);
+        a.insertar(70);
+        a.insertar(80);
+        a.insertar(100);
+        a.insertar(15);
+        a.insertar(22);
         a.insertar(21);
-        
+        a.imprimir();
+        a.eliminar();
         a.imprimir();
     }
 
